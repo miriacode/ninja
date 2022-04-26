403f94094c362f9280d527d006c5cfec9f0db53d
miriacode <miriamacunaenciso@gmail.com>
2022-04-26 16:02:07 -0500

    Completed exercise

---
+++ ninja.js
@@ -0,0 +1,28 @@
+class Ninja{
+    constructor(name){
+       this.name = name;
+       this.health = 0;
+       this.speed = 3;
+       this.strength = 3;
+    }
+
+    sayName(){
+        console.log(this.name);
+    }
+    
+    showStats(){
+        console.log("Name: " + this.name);
+        console.log("Strength: " + this.strength);
+        console.log("Speed: " + this.speed);
+        console.log("Health: " + this.health);
+    }
+
+    drinkSake(){
+        this.health += 10;
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.drinkSake();
+ninja1.showStats();
